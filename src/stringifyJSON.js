66a2fdaddec36e8af3f4bf0b55c0529ab6b5304a
kihong choi <khchoi0301@gmail.com>
2018-09-17 20:50:30 +0900

    Complete stringifyJSON

--- src/stringifyJSON.js
+++ src/stringifyJSON.js
@@ -4,7 +4,43 @@
 // but you don't so you're going to write it from scratch:
 
 var stringifyJSON = function(obj) {
-  // your code goes here
+  
+  console.log(obj)
+  var newObj;
+  var resArr=[];
+  var index;
+
+  if(index<0){
+    return;
+  }
+
+  if(typeof(obj)==='string'){
+    newObj = '"' + obj + '"';
+  } else if(obj===null || typeof(obj) === 'boolean' || typeof(obj) === 'number'){
+    newObj = obj + ""; 
+  } else if (Array.isArray(obj)){
+    index = obj.length;
+    for( var i = 0 ; i < obj.length ; i++ ){
+      resArr.push(stringifyJSON(obj[i]));
+    }
+    newObj = '[' + resArr.join(',') + ']';
+  } else if(typeof(obj)==='object'){
+    for(var key in obj){
+      if(key!=='undefined' && key!=='functions'){
+        resArr.push(stringifyJSON(key));
+        resArr.push(':');
+        resArr.push(stringifyJSON(obj[key]));
+        resArr.push(',');
+      }
+    }
+    resArr.pop();
+    newObj = '{' + resArr.join('') + '}';
+    return newObj;
+  } else {
+    newObj = obj + '';
+  }
+  index--;
+  return newObj;
 };
 
 if ( typeof module === "object" && typeof module.exports === "object" ) {
